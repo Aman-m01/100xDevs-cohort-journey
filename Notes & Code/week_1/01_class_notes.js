3a1aa02404f934a1982d0f3a0e79267c3ab69c0b
[Aman Maurya] <amank9thdnr@gmail.com>
2024-10-16 13:09:12 +0530

    Async js

--- Notes & Code/week_1/01_class_notes.js
+++ Notes & Code/week_1/01_class_notes.js
@@ -280,3 +280,36 @@ function onDone(data) {
 var a = kiratReadFile();
 console.log(a);
 a.then(onDone);
+
+
+/**
+ >> js architecture for async code 
+ 1. Call Stack
+-- The call stack is a data structure that keeps track of the function calls in your program. It operates in a "Last In, First Out" (LIFO) manner, meaning the last function that was called is the first one to be executed and removed from the stack.
+-- When a function is called, it gets pushed onto the call stack. When the function completes, it's popped off the stack.
+
+2. Web APIs
+-- Web APIs are provided by the browser (or the Node.js runtime) and allow you to perform tasks that are outside the scope of the JavaScript language itself, such as making network requests, setting timers, or handling DOM events
+
+3. Callback Queue 
+-- The callback queue is a list of tasks (callbacks) that are waiting to be executed once the call stack is empty. These tasks are added to the queue by Web APIs after they have completed their operation.
+
+4. Event loop
+-- The event loop constantly checks if the call stack is empty. If it is, and there are callbacks in the callback queue, it will push the first callback from the queue onto the call stack for execution.
+ */
+
+/*
+>> I/O heavy operations
+-- I/O (Input/Output) heavy operations refer to tasks in a computer program that involve a lot of data transfer between the program and external systems or devices. These operations usually require waiting for data to be read from or written to sources like disks, networks, databases, or other external devices, which can be time-consuming compared to in-memory computations.
+ex-
+       Reading a file
+       Starting a clock
+       HTTP Requests
+
+>> CPU bound tasks
+-- CPU-bound tasks are operations that are limited by the speed and power of the CPU. These tasks require significant computation and processing power, meaning that the performance bottleneck is the CPU itself.
+
+>> I/O bound tasks
+-- I/O-bound tasks are operations that are limited by the system’s input/output capabilities, such as disk I/O, network I/O, or any other form of data transfer. These tasks spend most of their time waiting for I/O operations to complete.
+ */
+
