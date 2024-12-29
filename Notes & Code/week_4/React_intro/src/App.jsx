ba8e12cab4048e599a9c16aca7dc57f9a6cc2494
[Aman Maurya] <amank9thdnr@gmail.com>
2024-12-29 21:54:06 +0530

    .

--- Notes & Code/week_4/React_intro/src/App.jsx
+++ Notes & Code/week_4/React_intro/src/App.jsx
@@ -9,12 +9,15 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+ <>
+ <h2>Counter App</h2>
+ <h3>Count {count}</h3>
+ <button onClick={()=>{setCount(count + 1)}}>Increase</button>
+ <button onClick={()=>{setCount(count - 1)}}>Decrease</button>
+ <button onClick={()=>{setCount(0)}}>Reset</button>
  
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+ 
+ </>
      
   )
 }
